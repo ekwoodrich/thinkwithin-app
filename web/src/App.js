198df9bbdcf91cb779e6f4e7d21c4d781011320d
Eliot Kent Woodrich <ekwoodrich@gmail.com>
2022-01-25 11:57:33 -0800

    Initial commit.

---
+++ web/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+        ThinkWithin
+        <div>
+          <textarea className="text-sm w-1/2 border-black border-solid border-2 m-8">
+
+          </textarea>
+        </div>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
